cc15bc62df615603013aa502f88d472450d992b4
Jonathan Ortega <jonathanortega2023@gmail.com>
2020-10-15 06:03:52 -0500

    GHappyTest implemented

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,15 @@
 package tudelft.ghappy;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    
+    @Test
+    public void single() {
+        GHappy gh = new GHappy();
+        assertFalse(gh.gHappy("g"));
+    }
 }
